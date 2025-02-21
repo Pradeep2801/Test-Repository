edca39e150d4e7c20e3ff729aca20a1b1c1ba5e9
p-pradeep.g <p-pradeep.g@metricstream.com>
2025-02-21 11:33:01 +0530

    This is the init commit

---
+++ Demo1/src/com/test/Main.java
@@ -0,0 +1,31 @@
+package com.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+	public static void main(String[] args) {
+		String obj = """
+				_org := root.organisation.add(Organisation, id := '200', name := 'Organisation A')_org1 := _org.add(Organisation, id := '101', name := 'Org B')_org1.add(Organisation, id := 'RTS_Organisation_106', name := 'OrgnB1')_org1.add(Organisation, id := 'RTS_Organisation_107', name := 'Org B2')_org4 := _org.add(Organisation, id := '111', name := 'Org C')
+				_sco := _org4.add(Scorecard, id := 'RTS_Scorecard_152', name := 'Scorecard 1')
+				        _sco.add(RiskChart, id := 'RTS_RiskChart_153', name := 'Risk chart 1')
+				        _sco.add(BarChart, id := 'RTS_BarChart_154', name := 'Bar chart 1')
+				_per := _sco.add(Perspective, id := '167')
+				_str := _per.add(StrategicObjective, id := '168')
+				        _str.add(Kpi, id := '169')
+				        _sco.add(StandardTable, id := '171')
+				        _sco.add(FilterTable, id := '172')
+				        _sco.add(DescriptionView, id := '173')
+				        _sco.add(LocalComments, id := '174')
+				        _sco.add(ActionButton, id := '175')
+				        _sco.add(RiskList, id := '176')""";
+
+		Pattern pattern = Pattern.compile("add\\((\\S+), id := '(\\S+)'");
+
+		Matcher matcher = pattern.matcher(obj);
+
+		String result = matcher.replaceAll("add($1, id := 'RTS_$1_$2'");
+
+		System.out.println(result);
+	}
+}
